a618f729bddbc4a120cf2ed51908adc34b49f08f
chrischism8063 <chrischism8063>
2019-06-27 20:53:21 -0400

    Pushing changes up to video 18. troubleshoot needed

--- marioplan/src/store/actions/projectActions.js
+++ marioplan/src/store/actions/projectActions.js
@@ -1,9 +1,19 @@
 export const createProject = (project) => {
     return (dispatch, getState, { getFirebase, getFirestore}) => {
         // make async call to database
-
-        dispatch({ type: 'CREATE_PROJECT', project: project });
+        const firestore = getFirestore();
+        // async
+        firestore.collection('projects').add({
+            ...project,
+            authorFirstName: 'Net',
+            authorLastName: 'Ninja',
+            authorId: 12345,
+            createdAt: new Date()
+        }).then(() => {
+            dispatch({ type: 'CREATE_PROJECT', project: project });
+        }).catch((err) => {
+            dispatch({ type: 'CREATE_PROJECT_ERROR', err });
+        });
+        
     }
 };
-
-// TODO: stopped at tutorial #17 5 minutes
--- marioplan/src/store/reducers/projectReducer.js
+++ marioplan/src/store/reducers/projectReducer.js
@@ -7,11 +7,19 @@ const initState = {
 }
 
 const projectReducer = (state = initState, action) => {
+    // eslint-disable-next-line
     switch(action.type){
         case 'CREATE_PROJECT':
             console.log('created project', action.project);
+            return state;
+        case 'CREATE_PROJECT_ERROR':
+            console.log('create project error', action.err);
+        default: 
+            return state;
     }
-    return state;
 }
 
 export default projectReducer;
+
+// TODO: verify video 18 which implements code to add projects into firebase 7:50 minutes
+//Unable to verify data added to firebase from site. Troubleshoot
